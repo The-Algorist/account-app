473c550403e53a5ae22b778ce9e279688c05b43d
Peter <peterjajakim@gmail.com>
2023-11-30 18:28:31 +0300

    ADDED LOGIN

--- my-account-app/src/App.js
+++ my-account-app/src/App.js
@@ -5,18 +5,32 @@ import Login from './components/Login';
 import Register from './components/Register';
 import Transactions from './components/Transactions';
 import AccountSettings from './components/AccountSettings';
+import { ClerkProvider } from "@clerk/clerk-react";
+ 
+if (!process.env.REACT_APP_CLERK_PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
+const clerkPubKey = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY;
+ 
 
 function App() {
   return (
+    <ClerkProvider publishableKey={clerkPubKey}>
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+     
         <Route path="/login" element={<Login />} />
+        
         <Route path="/register" element={<Register />} />
+
         <Route path="/transactions" element={<Transactions />} />
+       
         <Route path="/account-settings" element={<AccountSettings />} />
       </Routes>
     </Router>
+    </ClerkProvider>
+   
   );
 }
 
--- my-account-app/src/components/Login.js
+++ my-account-app/src/components/Login.js
@@ -1,51 +1,28 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import './Login.css';
+import { 
+  SignedIn,
+  SignedOut,
+  UserButton,
+   } from "@clerk/clerk-react";
+
+
+
 
 function Login() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
-
-  // Replaced useHistory with useNavigate
-  const navigate = useNavigate();
-
-  const handleLogin = () => {
-    // Implement login logic here
-    // For now, let's simulate a simple login
-    if (email === 'user@example.com' && password === 'password') {
-      // Simulate successful login
-      console.log('Login successful!');
-      setError('');
-
-      // Redirect to the home page or any desired route
-      // Replaced push with navigate
-      navigate('/');
-    } else {
-      // Simulate login error
-      setError('Invalid email or password. Please try again.');
-    }
-  };
+  
+  
 
   return (
-    <div>
-      <h2>Login</h2>
-      {error && <p style={{ color: 'red' }}>{error}</p>}
-      <form>
-        <label>Email:</label>
-        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-
-        <label>Password:</label>
-        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-
-        <button type="button" onClick={handleLogin}>
-          Login
-        </button>
-      </form>
-      <p>
-        Don't have an account? <Link to="/register">Register here</Link>.
-      </p>
-    </div>
+    
+   
+    <>
+    <h1>Protected page</h1>
+    <UserButton />
+  </>
+   
+   
   );
 }
 
