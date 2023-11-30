17c7a5b68a82b72ebb953bbdeafb17a9c6b4ba34
Warui Wanjiru <83602292+The-Algorist@users.noreply.github.com>
2023-11-30 10:06:17 +0000

    Added home page styling

--- my-account-app/src/components/Home.js
+++ my-account-app/src/components/Home.js
@@ -1,12 +1,28 @@
+// src/components/Home.js
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Home.css'; // Import a potential CSS file (create if needed)
 
-function Home() {
+const Home = () => {
   return (
-    <div>
-      <h1>Welcome to the Account Opening App</h1>
-      {/* Add links to navigate to other pages */}
+    <div className="home-container">
+      <h2>Welcome to the Account Opening App</h2>
+      <div className="home-buttons">
+        <Link to="/login">
+          <button>Login</button>
+        </Link>
+        <Link to="/register">
+          <button>Register</button>
+        </Link>
+        <Link to="/transactions">
+          <button>Transactions</button>
+        </Link>
+        <Link to="/account-settings">
+          <button>Account Settings</button>
+        </Link>
+      </div>
     </div>
   );
-}
+};
 
 export default Home;
