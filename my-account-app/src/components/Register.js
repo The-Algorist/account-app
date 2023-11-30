943c3415c7476402de7a13cafe34288513e8db84
Warui Wanjiru <83602292+The-Algorist@users.noreply.github.com>
2023-11-30 10:34:15 +0000

    Updated home page

--- my-account-app/src/components/Register.js
+++ my-account-app/src/components/Register.js
@@ -1,37 +1,61 @@
+// src/components/Register.js
 import React, { useState } from 'react';
-import './Register.css'; // Import the CSS file for styling
+import classNames from 'classnames';
+import './Register.css';
 
-function Register() {
+const Register = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   const handleRegister = () => {
     // Implement registration logic here
+    // For now, let's just log the values to the console
     console.log('Name:', name);
     console.log('Email:', email);
     console.log('Password:', password);
   };
 
+  const containerClass = classNames('register-container', 'max-w-md', 'mx-auto', 'p-8', 'border', 'rounded', 'shadow', 'bg-white');
+
   return (
-    <div>
-      <h2>Create an Account</h2>
+    <div className={containerClass}>
+      <h2 className="text-2xl font-bold mb-4">Create an Account</h2>
       <form>
-        <label>Name:</label>
-        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+        <label className="block mb-2 font-bold">Name:</label>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          className="w-full px-4 py-2 mb-4 border rounded"
+        />
 
-        <label>Email:</label>
-        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <label className="block mb-2 font-bold">Email:</label>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="w-full px-4 py-2 mb-4 border rounded"
+        />
 
-        <label>Password:</label>
-        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        <label className="block mb-2 font-bold">Password:</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="w-full px-4 py-2 mb-4 border rounded"
+        />
 
-        <button type="button" onClick={handleRegister}>
+        <button
+          type="button"
+          onClick={handleRegister}
+          className="w-full py-2 text-white bg-green-500 rounded cursor-pointer"
+        >
           Register
         </button>
       </form>
     </div>
   );
-}
+};
 
 export default Register;
